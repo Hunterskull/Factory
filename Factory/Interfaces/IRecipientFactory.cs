be26fc01b2bdc3ddde328f6e550c75d1b98d77bb
lucas letor <lucas.letor@gmail.com>
2020-05-07 20:32:27 +0200

    Add project files.

---
+++ Factory/Interfaces/IRecipientFactory.cs
@@ -0,0 +1,9 @@
+﻿using Factory.Models;
+
+namespace Factory.Interfaces
+{
+    public interface IRecipientFactory
+    {
+        public Recipient CreateRecipient(RecipientType type, string address);
+    }
+}
---
+++ Factory/Recipient.cs
@@ -0,0 +1,8 @@
+﻿namespace Factory
+{
+    public abstract class Recipient
+    {
+        protected string address = "undefined";
+        public string Address => address;
+    }
+}
